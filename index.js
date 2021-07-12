b12d39f6d48e58cffacc15844024dfe73087b021
DanH3re <41647014+DanH3re@users.noreply.github.com>
2021-07-12 20:43:58 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const Discord = require('discord.js')
 const client = new Discord.Client({ partials: ['USER', 'GUILD_MEMBER', 'CHANNEL', 'MESSAGE', 'REACTION'] });
 var prefix = '/'
 const token = 'ODE1MTc2NzUwNjk0NDAwMDEw.YDomgg.P8UPrj_ziAwT20GN4W157NmOur0';
+
+const queue = new Map();
+const ytdl = require('ytdl-core');
+const ytSearch = require('yt-search')
+
 client.login(token);
 client.on('ready', () => {
     console.log('Bot ON');
@@ -56,4 +61,178 @@ client.on("message", async message => {
 
   };
 
+  
+  const server_queue = queue.get(message.guild.id);
+  if(command === 'play'){
+  deletemessage()
+  const voiceChannel = message.member.voice.channel;
+  if(!voiceChannel) return message.channel.send('<:error:864199217559765032> **Вы должны быть в голосовом канале чтобы использовать эту команду!**');
+  const voicePerms = voiceChannel.permissionsFor(message.client.user);
+  if(!voicePerms.has('CONNECT')) return message.channel.send('<:error:864199217559765032> **У вас нет прав чтобы использовать данную команду!**');
+  if(!voicePerms.has('SPEAK')) return message.channel.send('<:error:864199217559765032> **У вас нет прав чтобы использовать данную команду!**');
+  if(!args.length) return message.channel.send('<:error:864199217559765032> **Введите название или ссылку на видео!**');
+  let song = {};
+
+  if(ytdl.validateURL(args[0])) {
+    const songInfo = await ytdl.getInfo(args[0]);
+    song = {title: songInfo.videoDetails.title, author: songInfo.videoDetails.author, url: songInfo.videoDetails.video_url}
+  } else {
+    const videoFinder = async (query) => {
+      const videoResult = await ytSearch(query);
+      return(videoResult.videos.length > 1) ? videoResult.videos[0]: null;
+    }
+    const video = await videoFinder(args.join(' '))
+    if(video) {
+      song = {title: video.title, author: video.author, url: video.url}
+    } else {
+      message.channel.send('<:error:864199217559765032> **Видео не найдено.**');
+    }
+  }
+
+if(!server_queue) {
+  const queueConstructor = {
+    voice_channel: voiceChannel,
+    text_channel: message.channel,
+    connection: null,
+    songs: []
+  };
+  queue.set(message.guild.id, queueConstructor)
+  queueConstructor.songs.push(song)
+
+  try {
+    const connection = await voiceChannel.join();
+    queueConstructor.connection = connection;
+    videoPlayer(message.guild, queueConstructor.songs[0])
+  } catch(err) {
+    queue.delete(message.guild.id)
+    message.channel.send('<:error:864199217559765032> **Ошибка при подключении.**');
+    console.log(err);
+  } 
+} else {
+  server_queue.songs.push(song);
+  message.channel.send(`<:queue:850726004297302046> **Песня __${song.title}__ добавлена в очередь на __${server_queue.songs.length}__ месте.**`);
+}
+
+} else if(command === 'stop'){
+    deletemessage()
+    stopSong(message, server_queue)
+} else if(command === 'skip'){
+    deletemessage()
+    skipSong(message, server_queue)
+  };
+
+if(command === 'd'){
+    isAdmin(message.member);
+    message.delete();
+    if(!args[0]) {
+      message.channel.send({
+          "embed": {
+              "title": "<:error:864199217559765032> Ошибка",
+              "description": "Недостаточно аргументов.",
+              "color": 16711680
+          }
+      })
+  } else if(args[0]<=100) {
+  message.channel.bulkDelete(args[0]).catch(error => {
+    if (error.code !== 0) {
+          var errorDescription = 'Что то пошло не так.\n\n' + '```' + error + '```'
+          message.channel.send({
+              "embed": {
+                  "title": "<:error:864199217559765032> Ошибка",
+                  "description": errorDescription,
+                  "color": 16711680
+              }
+          })
+      }
+  });
+    message.channel.send({
+        "embed": {
+            "title": "Успешно <:checkmark:850726004649099264>",
+            "description": `Удалил ${args[0]} сообщении.`,
+            "color": 7601920
+        }
+    })
+  } else if(args[0]<=500) {
+      var deletedMessages = 0;
+      message.channel.bulkDelete(100).catch(error => {
+          // Only log the error if it is not an Unknown Message error
+          if (error.code !== 0) {
+              var errorDescription = 'Что то пошло не так.\n\n' + '```' + error + '```'
+              message.channel.send({
+                  "embed": {
+                      "title": "<:error:864199217559765032> Ошибка",
+                      "description": errorDescription,
+                      "color": 16711680
+                  }
+              })
+          } else {
+              for (var i = 100; i < args[0];) {
+                  i = i + 100;
+                  message.channel.bulkDelete(100)
+                  deletedMessages = deletedMessages + 100;
+              }
+              message.channel.send({
+                  "embed": {
+                      "title": "Успешно <:checkmark:850726004649099264>",
+                      "description": `Удалил ${args[0]} сообщении.`,
+                      "color": 7601920
+                  }
+              })
+          }
+      });
+  
+      
+  
+  } else {
+      message.channel.send({
+          "embed": {
+              "title": "<:error:864199217559765032> Ошибка",
+              "description": "Введите номер от 0 до 500",
+              "color": 16711680
+          }
+      })
+  }
+}
+
 });
+
+const videoPlayer = async (guild, song) => {
+  const song_queue = queue.get(guild.id);
+    if (!song) {
+        song_queue.voice_channel.leave();
+        song_queue.text_channel.send(`<:queue:864199217739988992> **Очередь музыки закончилась.**`);
+        queue.delete(guild.id);
+        return;
+    }
+    const stream = ytdl(song.url, { filter: 'audioonly' });
+    song_queue.connection.play(stream, { seek: 0, volume: 0.5 })
+    .on('finish', () => {
+        song_queue.songs.shift();
+        videoPlayer(guild, song_queue.songs[0]);
+    });
+    await song_queue.text_channel.send(`<:music:864199218049712128> **Ебашу песню под названием - __${song.title}__**`);
+}
+
+const skipSong = (message, server_queue) => {
+  if(!message.member.voice.channel) return message.channel.send('<:error:864199217559765032> **Вы должны быть в голосовом канале чтобы использовать эту команду!**');
+  if(!server_queue) return message.channel.send('<:error:864199217559765032> **Ничего не было добавлено в очередь.**');
+  message.channel.send(`**<:skip:864199217626087474> Переключаюсь на следующею композицию.**`);
+  server_queue.connection.dispatcher.end();
+}
+
+const stopSong = (message, server_queue) => {
+  if(!message.member.voice.channel) return message.channel.send('<:error:864199217559765032> **Вы должны быть в голосовом канале чтобы использовать эту команду!**');
+  if(!server_queue) return message.channel.send('<:error:864199217559765032> **Ничего не было добавлено в очередь.**');
+  message.channel.send(`**<:stop:864199219237879828> Остановил проигравание всех песен.**`);
+  server_queue.song = [];
+  server_queue.connection.dispatcher.end();
+}
+
+function isAdmin(user) {
+  if(!user.hasPermission('ADMINISTRATOR')) return message.channel.send("<:error:864199217559765032> **У вас недостаточно прав!**")
+}
+
+function getRandomNumber(min,max){
+  return Math.floor(Math.random()*(max-min+1)+min);
+}
+
