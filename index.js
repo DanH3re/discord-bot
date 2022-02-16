dfe117a68f591eb03df0a22264459b09eb3b98d3
DanH3re <41647014+DanH3re@users.noreply.github.com>
2022-02-17 00:34:48 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -20,17 +20,31 @@ client.on('guildMemberAdd', member => {
   "embed": 
     {
       "title": "Добро пожаловать в **Rothschild Family**",
-      "description": "**В первую очередь получите роль:**\n<:role:939225432694341632>  [Выдача роли](https://discord.com/channels/810640147854000138/879279441103175721)\n\n**Полезные каналы**\n<:leader:939230416504754176> [Наши лидерки](https://discord.com/channels/810640147854000138/897498450504056852)\n<:partners:939231876843962469> [Наши партнеры](https://discord.com/channels/810640147854000138/896653447347195945)\n<:galleyy:939231581128765470> [Галерея](https://discord.com/channels/810640147854000138/939205663706468363)\n\n**Соц. Сети**\n<:youtube:939232239160528926> [YouTube](https://www.youtube.com/channel/UCq8BM7uQmBsWKyNLl2Ps0Tg)",
+      "description": "**В первую очередь получите роль:**\n<:role:939225432694341632>  [Выдача роли](https://discord.com/channels/810640147854000138/879279441103175721)\n\n**Полезные каналы**\n<:leader:939230416504754176> [Наши лидерки](https://discord.com/channels/810640147854000138/879279441103175721)\n<:partners:939231876843962469> [Наши партнеры](https://discord.com/channels/810640147854000138/879279441103175721)\n<:galleyy:939231581128765470> [Галерея](https://discord.com/channels/810640147854000138/879279441103175721)\n\n**Соц. Сети**\n<:youtube:939232239160528926> [YouTube](https://www.youtube.com/channel/UCq8BM7uQmBsWKyNLl2Ps0Tg)",
       "color": 16764416,
       "image": {
-        "url": `${image}`
+        "url": "https://cdn.discordapp.com/attachments/939205663706468363/939211464563097690/3.png"
       },
       "thumbnail": {
-        "url": "https://i.imgur.com/tRi7oL7.png"
+        "url": "https://cdn.discordapp.com/attachments/899321217838878793/918891527382642688/online-logo-generator-featuring-the-god-of-time-2920k_1.png"
       }
     }
 });
 });
+
+client.on('guildMemberRemove', async (member) => {
+  member.guild.channels.cache.get("842879008547012649").send(
+
+    {
+      "content": null,
+      "embed": 
+        {
+          "description": `:door: ${member} вышел с сервера **Rothschild Family**.`,
+          "color": 16711680
+  
+    }}
+  );
+ })
 
 client.on("message", async message => {
     if (message.author.bot) return;
